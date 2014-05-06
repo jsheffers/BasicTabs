33671dbf2aaa3670dc526103287c7bc62454512a
Shane Jeffers <shanejeffers@mac.com>
2014-05-06 16:07:19 -0400

    Added minified version & linking support.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 $(function(){
-	
-	$('#tabs li a').click(function(e){
-
-		$('#tabs li, #content .current').removeClass('current').removeClass('fadeInLeft');
-		$(this).parent().addClass('current');
-		var currentTab = $(this).attr('href');
-		$(currentTab).addClass('current');
-		e.preventDefault();
-
-	});
-
-
+  var url = location.hash.slice(1);
+  if(url){
+    $('.tabs li, .tab_content .current').removeClass('current').removeClass('fadeInLeft');
+    $('#' + url).addClass('current fadeInLeft');
+    $('.tabs li a[href=#' + url + ']').parent().addClass('current');
+  }
+  $('.tabs li a').click(function(e){
+      var clicked_url = location.hash.slice(1);
+      if( url !== clicked_url) { $('#' + clicked_url).addClass('current fadeInLeft'); }
+      $('.tabs li, .tab_content .current').removeClass('current').removeClass('fadeInLeft');
+      $(this).parent().addClass('current');
+      var currentTab = $(this).attr('href');
+      $(currentTab).addClass('current fadeInLeft');
+      e.preventDefault();
+  });
 });
+
+
 
