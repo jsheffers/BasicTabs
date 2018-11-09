ac0c0b87aa995551cec46c2bae3b4bad294cbd37
Shane Jeffers <jsheffers@gmail.com>
2018-11-09 13:17:39 -0500

    Update

--- js/basicTabs.js
+++ js/basicTabs.js
@@ -3,14 +3,13 @@
     var settings = $.extend({
       active_class: "is-active",
       open_class: "is-open",
-      list_class: "c-tabs",
       starting_tab: 1
     }, options );
 
-    $("." + settings.list_class).each(function() {
+    this.each(function() {
       $(this).children('li:nth-child(' + settings.starting_tab + ')' ).children('a').addClass(settings.active_class).next().addClass(settings.open_class).show();
-    });    
-    $("." + settings.list_class).on('click', 'li > a.c-tabs__link', function(event) {
+    });
+    this.on('click', 'li > a', function(event) {
       if (!$(this).hasClass(settings.active_class)) {
         event.preventDefault();
         var tabs = $(this).closest("." + settings.list_class);
